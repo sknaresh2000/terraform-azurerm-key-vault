533118e8ffdf32c98921dd15e5a3de388770fecd
NareshKumaran Sathiaseelan <naresh@nareshkransmini.mynetworksettings.com>
2023-03-28 15:22:19 -0400

    Fixed outputs

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "id" {
   description = "The id of the created key vault"
 }
 
-output "vault_uri " {
+output "vault_uri" {
   value       = azurerm_key_vault.kv.vault_uri
   description = "The URI of the Key Vault, used for performing operations on keys and secrets."
 }
